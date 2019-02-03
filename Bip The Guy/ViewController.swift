c5d8ed39b72a2e8de55bac995f19429a1172c3b7
Mark Lee <lee.mark0320@gmail.com>
2019-02-03 03:59:02 -0500

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by Mark Lee on 2/3/19.
+//  Copyright © 2019 Mark Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
